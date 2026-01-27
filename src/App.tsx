5f04cf2d89aa1415ab63b63ddd8602faa689614f
shahzain19 <cncdofficer2018@gmail.com>
2026-01-27 08:43:19 -0800

    fs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { Dashboard } from './Dashboard';
 import { Feed } from './Feed';
